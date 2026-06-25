93b63f3a3d01288006b29ef6bf72d9a48d0ff036
Alex Kutzke <alexkutzke@gmail.com>
2026-06-25 08:20:21 -0300

    fix: retrospecto faltando na seção upcoming

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -311,6 +311,9 @@ export function HomePage() {
                     homeFlag={teamMap.get(match.home_team_id)}
                     awayFlag={teamMap.get(match.away_team_id)}
                     stadiumName={stadiumMap.get(match.stadium)}
+                    homeForm={historyMap.get(match.home_team_id)}
+                    awayForm={historyMap.get(match.away_team_id)}
+                    globalOdds={oddsMap.get(match.id) || null}
                     onPredict={(h, a) => handlePredict(match.id, h, a)}
                     saving={savingId === match.id}
                   />
